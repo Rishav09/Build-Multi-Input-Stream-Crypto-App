917581592ea490f6b824a4e7e6dc0fbd36de618d
rishav sapahia <rishavsapahia@gmail.com>
2025-05-31 20:27:22 -0400

    Added environment variable to config file

--- services/trades/src/trades/main.py
+++ services/trades/src/trades/main.py
@@ -39,11 +39,16 @@ def run(
                     key=message.key
                     )
                 logger.info(f'Produced message to topic{topic.name}')
+                logger.info(f'Trade{event.model_dump()} pushed to kafka')
+#        
 if __name__ =='__main__':
-
-    api = KrakenAPI(product_ids=['BTC/USD'])
+    from trades.config import config
+    api = KrakenAPI(product_ids=config.product_ids)
     run(
-        kafka_broker_address='localhost:31234',
-        kafka_topic_name='trades',
+        #kafka_broker_address='localhost:31234',
+        #kafka_broker_address='kafka-e11b-kafka-bootstrap.kafka.svc.cluster.local:9092',
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_topic_name=config.kafka_topic_name,
+        #kafka_topic_name='trades',
         kraken_api=api
     )
